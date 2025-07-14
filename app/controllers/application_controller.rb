a6bca2e5d83b8a94f3e44f3244ab827d9305146a
kousuke-jinnouchi <215395544+kousuke-jinnouchi@users.noreply.github.com>
2025-07-14 03:39:19 +0000

    [Finish] 4-24

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,9 @@ class ApplicationController < ActionController::Base
   protected 
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) 
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email]) 
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
 end
